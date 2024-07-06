c6a583f3dcd239e0ccfb94b86326206d290c1df0
syedBahadarKhan <ba4646465@gmail.com>
2024-07-06 00:46:24 -0700

    add this

---
+++ new.js
@@ -0,0 +1,7 @@
+/*
+// 1. TABLE PRINT KARE
+// DESCRIPTION: user se koi number le and uska table 10 tk print kare*/
+let num= +prompt("please enter a number");
+for(let i=1; i<=10; i++){
+    console.log(`${num} * ${i}= ${num *i}`);
+}
